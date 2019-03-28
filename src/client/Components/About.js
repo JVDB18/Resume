2f2aaaf5656c99efc865cb8d7b61c8ae59b2052d
JVDB18 <vanderbysej@gmail.com>
2019-03-28 10:00:02 +0100

    fix typo

--- src/client/Components/About.js
+++ src/client/Components/About.js
@@ -12,20 +12,20 @@ class About extends React.Component{
                             <img src={Pic2} className="ProfilePic"/>
                             <p className="AboutText">
                             <h3>Hi fellow human!</h3><br/>
-                            Let me introduce myself: Julie, Junior Front-End Developer currently looking for a fresh new internship / hiring opportunity.
+                            Let me introduce myself: Julie, <strong>Junior Front-End Developer currently looking for a fresh new internship / hiring opportunity.</strong>
                             I am a former nursing student who's taking a career move by pursuing a programming cursus at BeCode, a coding school based on peer-learning and learning by doing (Group's projects on a daily schedule, Hackatons, Coder Dojos).
                                 <br/>
                                 <br/>
                             At this point you are questionning yourself: <i>"What a freshman junior dev / former nurse can do in my company? "</i>
                                 
                                 <br/>
-                                First thing i would tell you is that with enthusiasm, passion, and a little bit of ambition's adrenaline, you can accomplish anything. <p className="whisper">- Thanks to the little chemist in your head - </p>
-                            And i have plenty of it.
-                            Perpetual learner, not affraid about working hard and late, when i started my transition in october, i barely had some HTML5 and CSS3 skills, back to this day  I could not imagine the path that would be traveled, nor the number of Hard and Soft skills that i would gain.
+                                First thing I would tell you is that with enthusiasm, passion, and a little bit of ambition's adrenaline, you can accomplish anything. <p className="whisper">- Thanks to the little chemist in your head - </p>
+                            And I have plenty of it.
+                            Perpetual learner, not affraid about working hard and late, when I started my transition in October, I barely had some HTML5 and CSS3 skills, back to this day  I could not imagine the path that would be traveled, nor the number of Hard and Soft skills that I would gain.
                             These last months have been for me a total mind-blowing revolution:
-                            I have learned to learn, in a fast and efficient way, making of my time a valuable product. Isn't it the coolest superpower an employe could have?
+                            I have learned to learn, in a fast and efficient way, making of my time a valuable product. Isn't it the coolest superpower an employee could have?
                                 <br/>
-                            Did i catch your attention? Just scroll down!
+                            Did I catch your attention? Just scroll down!
                             </p>
                 </div>
         
--- src/client/Components/Learn.js
+++ src/client/Components/Learn.js
@@ -6,11 +6,11 @@ class Learn extends React.Component{
             <h2 className="Title4">- Learn More -</h2>
             <p className="LearnText">
             <h3>Passions</h3>
-When i’m not banging my head against some code, you will find me with a pen and a piece of sheet, i’ve made my college education in art and still like to spend time creating illustration, painting or writing.
+When I’m not banging my head against some code, you will find me with a pen and a piece of sheet, I’ve made my college education in art and still like to spend time creating illustrations, painting or writing.
 
-Passionate about ocean, nature and wildlife hiking and diving also take a great part in my life, i am strongly involved in marine conservation since my first diving trip and totally fall in love with the philosophy, sensation and mindset of this discipline.
+Passionate about ocean, nature and wildlife hiking and diving also take a great part in my life. I am strongly involved in marine conservation since my first diving trip and totally fell in love with the philosophy, sensation and mindset of this discipline.
             <h3>Some facts  about me</h3>
-    <p>I can be stubborn as hell, i play video games since my dad showed me Diablo back to 1997, i loved Star Wars's prequel (i know, nobody is perfect...), i prefer Stargate over Battlestar Galactica, pineapple on pizza doesn't bother me, my friends thinks i have the sense of humor of a 3 years old, a simple smile can make my whole day, i'm a pomodoro and deep work adept, team work, curiosity and resourcefulness are essential to me, i have a cocker spaniel named Tania but everyone calls her Big Butt, and finally i recently discovered hackatons, challenging myself in those events were an incredible experience, now i'm addict.</p>
+    <p>I can be stubborn as hell. I play video games since my dad showed me Diablo back to 1997. I loved Star Wars's prequel (I know, nobody is perfect...). I prefer Stargate over Battlestar Galactica. Pineapple on pizza doesn't bother me. My friends thinks I have the sense of humor of a kid. A simple smile can make my whole day. I'm a Pomodoro and Deep work adept, team work, curiosity and resourcefulness are essential to me. I have a cocker spaniel named Tania but everyone calls her Big Butt, and finally I recently discovered hackatons, challenging myself in those events were an incredible experience, now I'm addict.</p>
     </p>
         </div>
     }
